40096a03a38df2d088d6bf3a76e66ca6460c1289
SaiHarish820 <2100031942@kluniversity.in>
2024-08-18 15:47:04 +0530

    MongoDB connected and now passwords are stored in hash form in database table

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,52 @@
-export const signup = async (req,res) => {
-    try{
-    const {fullName, userName, password, confirmPassword, gender} = req.body;
-    }
-    catch(error){
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
+
+export const signup = async (req,res) => {
+    try {
+        const { fullName, username, password, confirmPassword, gender } = req.body;
+    
+        if (password !== confirmPassword) {
+            return res.status(400).json({ Error: "Passwords don't match" });
+        }
+    
+        const user = await User.findOne({ username });
+    
+        if (user) {
+            return res.status(400).json({ Error: "Username already exists" });
+        }
+    
+        // HASH THE PASSWORD
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+    
+        // https://avatar-placeholder.iran.liara.run/
+        const boyProfilePic = `https://avatar-placeholder.iran.liara.run/${username}`;
+        const girlProfilePic = `https://avatar-placeholder.iran.liara.run/${username}`;
+    
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+    
+        await newUser.save();
+    
+        res.status(201).json({
+            _id: newUser.id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            password: newUser.password,
+            profilePic: newUser.profilePic,
+        });
+    
+    } catch (error) {
+        console.log("Error in Signup controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
     }
+    
 };
 
 export const login = (req,res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
-import express from "express";
+ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import connectToMongoDb from "./db/connectToMongoDB.js";
 
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json()); // to parse the incoming requests with json payloads (from req.body)
+
 
 app.get("/", (req,res) => {
     // root route https://localhost:5000/
-    res.send("Hello World!!");
+    res.send("Hello World!!!!");
 
 });
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
+app.listen(PORT, () => {
+    connectToMongoDb();
+    console.log(`Server is running at port ${PORT}`)
+});
