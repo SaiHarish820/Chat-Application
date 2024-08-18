0d1bbc3c07792a9a97410c366e04f256932e378a
SaiHarish820 <2100031942@kluniversity.in>
2024-08-18 12:35:53 +0530

    Basic env setup, added neccessary packages, added mongo url

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+export const signup = async (req,res) => {
+    try{
+    const {fullName, userName, password, confirmPassword, gender} = req.body;
+    }
+    catch(error){
+
+    }
+};
+
+export const login = (req,res) => {
+   
+};
+
+export const logout = (req,res) => {
+    
+};
