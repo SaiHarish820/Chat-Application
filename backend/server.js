61363afa71c4b2db6ea485b825c6e603375f99bd
SaiHarish820 <2100031942@kluniversity.in>
2024-08-23 15:34:05 +0530

    Now deploy at Render

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,30 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import { app, server } from "./socket/socket.js";
+import path from "path"
 
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with json payloads (from req.body)
 app.use(cookieParser());
 
-
-//app.get("/", (req,res) => {
-//    // root route https://localhost:5000/
-//    res.send("Hello World!!!!");
-//
-//});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDb();
